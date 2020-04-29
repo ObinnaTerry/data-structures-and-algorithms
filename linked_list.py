be25f74c6aaf74db233ef9830145517814171cb5
youngjay90 <3254048915@qq.com>
2020-04-29 11:17:45 +0200

    feat: Create Node class

--- linked_list.py
+++ linked_list.py
@@ -9,3 +9,10 @@ class LinkedList:
             return 'List is empty'
 
 
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+    def __repr__(self):
+        return self.data
