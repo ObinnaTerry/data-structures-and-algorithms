6500f187759eee548c2728192d11144d08e1996e
youngjay90 <3254048915@qq.com>
2020-05-02 13:13:47 +0200

    refactor: Change the node length calculation to instead keep track of total nodes during creation

--- linked_list.py
+++ linked_list.py
@@ -1,37 +1,38 @@
 class LinkedList:
     def __init__(self, nodes=None):
         self.head = None
+        self.count = {'len': 0}
         if nodes is not None and len(nodes) != 0:
             node = Node(data=nodes.pop(0))
             self.head = node
+            self.count['len'] += 1
             for elem in nodes:
                 node.next = Node(data=elem)
                 node = node.next
+                self.count['len'] += 1
+        if nodes is not None and nodes == []:
+            raise Exception("Cant't create an empty head node")
 
     def insert_head(self, node):
         node.next = self.head
         self.head = node
+        self.count += 1
 
     def insert_tail(self, node):
         node_ = self.head
         if not node_:
             self.head = node
+            self.count['len'] += 1
             return
         while True:
             if node_.next is None:
                 node_.next = node
+                self.count['len'] += 1
                 break
             node_ = node_.next
 
     def __len__(self):
-        node = self.head
-        count = 0
-        if node is None:
-            return 0
-        while node is not None:
-            count += 1
-            node = node.next
-        return count
+        return self.count['len']
 
     def __iter__(self):
         node = self.head
@@ -69,7 +70,60 @@ class Node:
 # two.next = three
 # llist = LinkedList()
 # llist.head = one
-# llist = LinkedList(['a', 'b', 'c'])
-# n = Node('d')
-# llist.insert_tail(n)
-# print(llist)
+llist = LinkedList(['a', 'b', 'c'])
+n = Node('d')
+llist.insert_tail(n)
+print(llist)
+print(len(llist))
+# def mergeSort(arr):
+#     if len(arr) > 1:
+#         mid = len(arr) // 2  # Finding the mid of the array
+#         L = arr[:mid]  # Dividing the array elements
+#         R = arr[mid:]  # into 2 halves
+#         print(L)
+#         print(R)
+#
+#         mergeSort(L)  # Sorting the first half
+#         mergeSort(R)  # Sorting the second half
+#
+#         i = j = k = 0
+#
+#         # Copy data to temp arrays L[] and R[]
+#         while i < len(L) and j < len(R):
+#             print('here')
+#             if L[i] < R[j]:
+#                 arr[k] = L[i]
+#                 i += 1
+#             else:
+#                 arr[k] = R[j]
+#                 j += 1
+#             print(arr)
+#             k += 1
+#
+#         # Checking if any element was left
+#         while i < len(L):
+#             arr[k] = L[i]
+#             i += 1
+#             k += 1
+#
+#         while j < len(R):
+#             arr[k] = R[j]
+#             j += 1
+#             k += 1
+#
+#
+# # Code to print the list
+# def printList(arr):
+#     for i in range(len(arr)):
+#         print(arr[i], end=" ")
+#     print()
+#
+#
+# # driver code to test the above code
+# if __name__ == '__main__':
+#     arr = [12, 11, 13, 5, 6, 7]
+#     print("Given array is", end="\n")
+#     printList(arr)
+#     mergeSort(arr)
+#     print("Sorted array is: ", end="\n")
+#     printList(arr)
