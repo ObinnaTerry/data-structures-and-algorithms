c6809bab065fe1a253d7fcf44626d07704060525
youngjay90 <3254048915@qq.com>
2020-05-03 16:25:32 +0200

    feat: Add push method for adding item to the stack

--- stack.py
+++ stack.py
@@ -1,6 +1,13 @@
 class Stack:
 
-    def __init__(self, size=None, ):
+    def __init__(self, size=None):
         self.size = size if size is not None else 100
+        self.stack = [None] * self.size  # initialize an empty list of length size
         self.top = -1
 
+    def push(self, num):
+        if self.top == self.size - 1:
+            raise Exception("Error: Stack Overflow")
+        self.top += 1
+        self.stack[self.top] = num
+        return
