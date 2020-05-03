34611ef26c819e07fac58d9cc7df41e43859bcaf
youngjay90 <3254048915@qq.com>
2020-05-03 16:50:38 +0200

    feat: Add is_empty method for checking if a stack is empty or not

--- stack.py
+++ stack.py
@@ -3,18 +3,38 @@ class Stack:
     def __init__(self, size=None):
         self.size = size if size is not None else 100
         self.stack = [None] * self.size  # initialize an empty list of length size
-        self.top = -1
+        self.top_ = -1
 
     def push(self, num):
-        if self.top == self.size - 1:
+        if self.top_ == self.size - 1:
             raise Exception("Error: Stack Overflow")
-        self.top += 1
-        self.stack[self.top] = num
+        self.top_ += 1
+        self.stack[self.top_] = num
         return
 
     def pop(self):
-        if self.top == -1:
+        if self.top_ == -1:
             raise Exception("Error: Stack is empty")
-        value = self.stack[self.top]
-        self.top -= 1
+        value = self.stack[self.top_]
+        self.top_ -= 1
         return value
+
+    def top(self):
+        return self.stack[self.top_]
+
+    def is_empty(self):
+        return True if self.top_ == -1 else False
+
+    def __repr__(self):
+        return str(self.stack[:self.top_+1])
+
+
+stack = Stack()
+d = Stack()
+# stack.pop()
+stack.push(3)
+stack.push(7)
+stack.push(32)
+# d.push(4)
+print(d.is_empty())
+print(stack)
